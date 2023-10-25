872a7e2b8928473292067289694d05c224f68d23
diomasaki <sendeross99@gmail.com>
2023-10-25 16:19:12 +0700

    Updates

---
+++ precache-manifest.2397e2f8b0e38f6c8f2421696a3cc63e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e404d0e5063a9fe8cdfe8ca704d2920",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "62810b112be6335c7f08",
+    "url": "/portfolio/static/css/main.9f24390c.chunk.css"
+  },
+  {
+    "revision": "932605bd66488f53121a",
+    "url": "/portfolio/static/js/2.33603be6.chunk.js"
+  },
+  {
+    "revision": "62810b112be6335c7f08",
+    "url": "/portfolio/static/js/main.bd00b8b9.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "a23093fbfc09d373ba3e73bfee764a7b",
+    "url": "/portfolio/static/media/address.a23093fb.png"
+  },
+  {
+    "revision": "ddf1c65efb6df4f8a98201743d9c7808",
+    "url": "/portfolio/static/media/email.ddf1c65e.png"
+  },
+  {
+    "revision": "aaa84aa6798433a32eea6f956bd67f5f",
+    "url": "/portfolio/static/media/masaki.aaa84aa6.png"
+  },
+  {
+    "revision": "76c7a84427308345ee01f4b2672c6005",
+    "url": "/portfolio/static/media/me.76c7a844.png"
+  },
+  {
+    "revision": "8298e1240630689c4c3af0da82f074af",
+    "url": "/portfolio/static/media/moon.8298e124.png"
+  },
+  {
+    "revision": "cffab408c70747ce5bd19151755448bb",
+    "url": "/portfolio/static/media/phone.cffab408.png"
+  },
+  {
+    "revision": "3331f574833e84ca82754d8b8c561fdc",
+    "url": "/portfolio/static/media/rk1.3331f574.png"
+  },
+  {
+    "revision": "4db06b748c95601d12529207e7f84b17",
+    "url": "/portfolio/static/media/rk2.4db06b74.png"
+  },
+  {
+    "revision": "eae4c4c778c9495e2fb0f23cefee1588",
+    "url": "/portfolio/static/media/sun.eae4c4c7.png"
+  }
+]);
